672d8b06a9414bda1ed315ab7fe5e33618a7f234
rohan <halo.zero00@gmail.com>
2015-01-18 20:46:00 -0800

    Adding rudimentary auto generation of index.html for main paeg

--- setup-presentation-slides.rb
+++ setup-presentation-slides.rb
@@ -1,6 +1,7 @@
+require 'haml'
+
 day_folders = Dir["source_slides/*"]
 system 'rm -rf presentation_slides/*'
-
 day_folders.each do |day_folder|
   new_folder_name = day_folder.gsub('source_slides', 'presentation_slides')
   system "mkdir #{new_folder_name}"
@@ -8,4 +9,16 @@ day_folders.each do |day_folder|
   system "ruby haml-templater.rb template.haml #{new_folder_name}/index.html"
   system "ruby slide-imager-replace.rb #{new_folder_name}/slides.md"
 end
+
+template = File.read('main-file-template.haml')
+haml_engine = Haml::Engine.new(template)
+
+presentation_folders = day_folders.map { |folder|
+  folder.gsub('source_slides', 'presentation_slides')
+}
+arguments = {directories: presentation_folders}
+file = File.open('index.html', 'w')
+file.write(haml_engine.render(Object.new, arguments))
+file.close
+
 
